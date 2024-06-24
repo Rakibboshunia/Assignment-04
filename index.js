2ee5acf8bf8acf369f295374ecaaa610c9c9329e
MD AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2024-06-24 23:35:13 +0600

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const dotEnv = require('dotenv');
 dotEnv.config({
         path: './config.env'
     })
-    //const port = 3000;
+    
+//const port = 3000;
 app.listen(process.env.RUNNING_PORT, function() {
     console.log("server is running with port " + process.env.RUNNING_PORT)
 });
